c7d1b83bfdc69b07898c54ea641467495cf566e0
Fred M <frederic.maquet@gmail.com>
2017-09-03 16:54:32 +0200

    Retour sur HelloWorld pour la creation du premier module

--- src/norsys/main/Main.java
+++ src/norsys/main/Main.java
@@ -1,12 +1,9 @@
 package norsys.main;
 
-import common.math.*;
-
 public class Main{
 
 	public static void main(String args[]){
 		String helloWorld = new String("Hello World ! ");
-		Calculatrice calculatrice = new Calculatrice();
-		System.out.println(helloWorld + calculatrice.somme(1, 1));
+		System.out.println(helloWorld);
 	}
 }
